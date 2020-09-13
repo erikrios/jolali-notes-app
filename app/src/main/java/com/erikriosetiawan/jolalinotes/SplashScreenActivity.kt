0362d5932031b8dc19e42c830ab8587b270e5dd7
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-13 20:35:13 +0700

    Add status bar color for splash screen

--- app/src/main/java/com/erikriosetiawan/jolalinotes/SplashScreenActivity.kt
+++ app/src/main/java/com/erikriosetiawan/jolalinotes/SplashScreenActivity.kt
@@ -9,6 +9,8 @@ class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        setStatusBar()
     }
 
     // Change the status bar color
