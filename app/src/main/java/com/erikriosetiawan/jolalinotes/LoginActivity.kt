fdff9edc44a63ea23619f51849d2a440a11d5077
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-13 21:33:45 +0700

    Add text click handler

--- app/src/main/java/com/erikriosetiawan/jolalinotes/LoginActivity.kt
+++ app/src/main/java/com/erikriosetiawan/jolalinotes/LoginActivity.kt
@@ -1,11 +1,17 @@
 package com.erikriosetiawan.jolalinotes
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+
+    fun registerAccount(view: View) {
+        Toast.makeText(this, "Register Account clicked!", Toast.LENGTH_SHORT).show()
     }
 }
