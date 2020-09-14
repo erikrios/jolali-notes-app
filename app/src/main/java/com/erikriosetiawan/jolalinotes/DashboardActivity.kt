47e2ae36ee0bb3c571d6647c49f540e437d58c0a
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-14 13:57:24 +0700

    Integrate the view binding

--- app/src/main/java/com/erikriosetiawan/jolalinotes/DashboardActivity.kt
+++ app/src/main/java/com/erikriosetiawan/jolalinotes/DashboardActivity.kt
@@ -2,10 +2,14 @@ package com.erikriosetiawan.jolalinotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.erikriosetiawan.jolalinotes.databinding.ActivityDashboardBinding
+
+private lateinit var binding: ActivityDashboardBinding
 
 class DashboardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard)
+        binding = ActivityDashboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
