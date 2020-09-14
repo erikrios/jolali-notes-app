6426ced3c30cdddcb5da06a13089228810964982
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-14 15:19:46 +0700

    Implements RecyclerView

--- app/src/main/java/com/erikriosetiawan/jolalinotes/DashboardActivity.kt
+++ app/src/main/java/com/erikriosetiawan/jolalinotes/DashboardActivity.kt
@@ -1,8 +1,11 @@
 package com.erikriosetiawan.jolalinotes
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.erikriosetiawan.jolalinotes.adapters.NoteAdapter
 import com.erikriosetiawan.jolalinotes.databinding.ActivityDashboardBinding
+import com.erikriosetiawan.jolalinotes.models.Note
 
 private lateinit var binding: ActivityDashboardBinding
 
@@ -11,5 +14,32 @@ class DashboardActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityDashboardBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val notes = setDummyData()
+        setRecyclerView(notes)
+    }
+
+    private fun setRecyclerView(notes: List<Note>) {
+        val noteAdapter = NoteAdapter(this, notes)
+        noteAdapter.setOnItemClickListener { note ->
+            Toast.makeText(this, note.title, Toast.LENGTH_SHORT).show()
+        }
+
+        binding.rvNotes.adapter = noteAdapter
+    }
+
+    private fun setDummyData(): List<Note> {
+        val notes = mutableListOf<Note>()
+
+        for (i in 1..99) {
+            val note = Note(
+                "Lorem Ipsum Dolor $i",
+                "$i Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                "2020/09/$i"
+            )
+            notes.add(note)
+        }
+
+        return notes
     }
 }
