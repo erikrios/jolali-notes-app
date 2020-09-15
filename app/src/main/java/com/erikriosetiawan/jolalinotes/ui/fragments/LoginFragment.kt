0ac0a7409a02a0aae64cd2a81d4d79e1abb4771a
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-15 14:00:45 +0700

    Navigating into other fragment

--- app/src/main/java/com/erikriosetiawan/jolalinotes/ui/fragments/LoginFragment.kt
+++ app/src/main/java/com/erikriosetiawan/jolalinotes/ui/fragments/LoginFragment.kt
@@ -5,8 +5,8 @@ import android.text.Html
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.erikriosetiawan.jolalinotes.R
 import com.erikriosetiawan.jolalinotes.databinding.FragmentLoginBinding
 
@@ -28,11 +28,7 @@ class LoginFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         setTextRegisterColor()
         binding?.btnLogin?.setOnClickListener {
-            Toast.makeText(
-                context,
-                "Login Button clicked!",
-                Toast.LENGTH_SHORT
-            ).show()
+            findNavController().navigate(R.id.action_loginFragment_to_dashboardFragment)
         }
     }
 
@@ -42,7 +38,7 @@ class LoginFragment : Fragment() {
     }
 
     fun registerAccount(view: View) {
-        Toast.makeText(context, "Register Account clicked!", Toast.LENGTH_SHORT).show()
+        findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
     }
 
     private fun setTextRegisterColor() {
