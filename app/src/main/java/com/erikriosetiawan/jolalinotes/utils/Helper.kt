936397f257f9734b50bfb5cc728965e1fd1774a2
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-15 20:22:19 +0700

    Create Helper.kt

---
+++ app/src/main/java/com/erikriosetiawan/jolalinotes/utils/Helper.kt
@@ -0,0 +1,11 @@
+package com.erikriosetiawan.jolalinotes.utils
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+
+fun AppCompatActivity.setCustomActionBar(toolbar: Toolbar?, title: String) {
+    this.apply {
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = title
+    }
+}
