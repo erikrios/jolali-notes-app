e95dd846cab2491a7822e7e25380d05745d995db
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-15 10:22:31 +0700

    Create Main Activity

---
+++ app/src/main/java/com/erikriosetiawan/jolalinotes/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.erikriosetiawan.jolalinotes.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.erikriosetiawan.jolalinotes.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
