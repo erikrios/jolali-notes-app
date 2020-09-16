7e88ce2363652213b497cff83e0445b8dc07d75b
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-16 18:55:22 +0700

    Add some notes request method to API

--- app/src/main/java/com/erikriosetiawan/jolalinotes/services/NotesService.kt
+++ app/src/main/java/com/erikriosetiawan/jolalinotes/services/NotesService.kt
@@ -1,5 +1,6 @@
 package com.erikriosetiawan.jolalinotes.services
 
+import com.erikriosetiawan.jolalinotes.models.Note
 import com.erikriosetiawan.jolalinotes.models.User
 import retrofit2.Call
 import retrofit2.http.*
@@ -35,4 +36,53 @@ interface NotesService {
         @Field("email") email: String,
         @Field("password") password: String
     ): Call<String>
+
+    /**
+     * Create a note
+     */
+    @POST("api/notes")
+    @FormUrlEncoded
+    fun createNote(
+        @Header("Auth-Token") token: String,
+        @Field("title") title: String,
+        @Field("description") description: String
+    ): Call<Note>
+
+    /**
+     * Get all notes
+     */
+    @GET("api/notes")
+    fun getNotes(
+        @Header("Auth-Token") token: String
+    ): Call<List<Note>>
+
+    /**
+     * Get a note by id
+     */
+    @GET("api/notes/{id}")
+    fun getNote(
+        @Path("id") id: String,
+        @Header("Auth-Token") token: String
+    ): Call<Note>
+
+    /**
+     * Update a note
+     */
+    @PUT("api/notes/{id}")
+    @FormUrlEncoded
+    fun updateNote(
+        @Path("id") id: String,
+        @Header("Auth-Token") token: String,
+        @Field("title") title: String,
+        @Field("description") description: String
+    ): Call<Note>
+
+    /**
+     * Delete a note
+     */
+    @DELETE("api/notes/{id}")
+    fun deleteNote(
+        @Path("id") id: String,
+        @Header("Auth-Token") token: String
+    ): Call<Note>
 }
