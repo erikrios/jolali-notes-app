300daec1081127120b4a61d3f06737c012e9d1ea
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-16 18:36:08 +0700

    Add some properties and override toString method

--- app/src/main/java/com/erikriosetiawan/jolalinotes/models/Note.kt
+++ app/src/main/java/com/erikriosetiawan/jolalinotes/models/Note.kt
@@ -1,7 +1,22 @@
 package com.erikriosetiawan.jolalinotes.models
 
 data class Note(
+    var _id: String,
     var title: String,
     var description: String,
-    var date: String
-)
+    var date: String,
+    var time: String,
+    var ownerId: NoteOwner
+) {
+    override fun toString(): String {
+        return """
+            _id: ${this._id}
+            title: ${this.title}
+            description: ${this.description}
+            date: ${this.date}
+            time: ${this.time}
+            ownerId._id: ${this.ownerId._id}
+            ownerId.name: ${this.ownerId.name}
+        """.trimIndent()
+    }
+}
