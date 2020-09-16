f761e10105f61647707ffac3bd58ac3904f37a08
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-16 14:06:24 +0700

    Test the register user API request

--- app/src/main/java/com/erikriosetiawan/jolalinotes/ui/MainActivity.kt
+++ app/src/main/java/com/erikriosetiawan/jolalinotes/ui/MainActivity.kt
@@ -1,12 +1,45 @@
 package com.erikriosetiawan.jolalinotes.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.erikriosetiawan.jolalinotes.R
+import com.erikriosetiawan.jolalinotes.models.User
+import com.erikriosetiawan.jolalinotes.repository.NotesRepository
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var repository: NotesRepository
+    private var token: String? = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        repository = NotesRepository()
+    }
+
+    private fun registerUser(name: String, email: String, password: String) {
+        val requestCall =
+            repository.registerUser("Erik Android", "erikandroid@gmail.com", "Erik1997")
+        requestCall.enqueue(object : Callback<User> {
+            override fun onResponse(call: Call<User>, response: Response<User>) {
+                if (response.isSuccessful) {
+                    val user = response.body()
+                    Log.i("API Body Response", user.toString())
+
+                    // Get headers
+                    val headers = response.headers()
+                    token = headers["Auth-Token"];
+                }
+            }
+
+            override fun onFailure(call: Call<User>, t: Throwable) {
+                t.printStackTrace()
+            }
+        })
     }
 }
