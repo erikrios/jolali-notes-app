8bb2dde2021270074d78f6f99029f0ad33e06337
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-16 13:55:54 +0700

    Add FormUrlEncoded annotation

--- app/src/main/java/com/erikriosetiawan/jolalinotes/services/NotesService.kt
+++ app/src/main/java/com/erikriosetiawan/jolalinotes/services/NotesService.kt
@@ -3,6 +3,7 @@ package com.erikriosetiawan.jolalinotes.services
 import com.erikriosetiawan.jolalinotes.models.User
 import retrofit2.Call
 import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
 
 interface NotesService {
@@ -11,6 +12,7 @@ interface NotesService {
      * POST Request to register a user
      */
     @POST("api/users")
+    @FormUrlEncoded
     fun registerUser(
         @Field("name") name: String,
         @Field("email") email: String,
