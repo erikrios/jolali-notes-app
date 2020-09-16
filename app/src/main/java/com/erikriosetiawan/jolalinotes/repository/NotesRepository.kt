219a3017c1c8470bcc36392002cf8e423285564f
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-16 19:01:35 +0700

    Create some method to request an API in the NotesRepository.kt

--- app/src/main/java/com/erikriosetiawan/jolalinotes/repository/NotesRepository.kt
+++ app/src/main/java/com/erikriosetiawan/jolalinotes/repository/NotesRepository.kt
@@ -14,4 +14,16 @@ class NotesRepository {
 
     fun authenticateUser(email: String, password: String) =
         notesService.authenticateUser(email, password)
+
+    fun createNote(token: String, title: String, description: String) =
+        notesService.createNote(token, title, description)
+
+    fun getNotes(token: String) = notesService.getNotes(token)
+
+    fun getNote(id: String, token: String) = notesService.getNote(id, token)
+
+    fun updateNote(id: String, token: String, title: String, description: String) =
+        notesService.updateNote(id, token, title, description)
+
+    fun deleteNote(id: String, token: String) = notesService.deleteNote(id, token)
 }
