53321d897b1d8b127970361ae4d54cb94b9cf3fb
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-16 12:18:54 +0700

    Create User.kt

--- app/src/main/java/com/erikriosetiawan/jolalinotes/models/User.kt
+++ app/src/main/java/com/erikriosetiawan/jolalinotes/models/User.kt
@@ -3,7 +3,7 @@ package com.erikriosetiawan.jolalinotes.models
 data class User(
     var _id: String,
     var name: String,
-    var email: String = "",
+    var email: String,
     var dateRegistered: String = "",
     var lastLogin: String = "",
 )
