4599321ef68416798d551ccf49188f5573f60fcb
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-17 10:04:00 +0700

    Crate LoginViewState.kt

--- app/src/main/java/com/erikriosetiawan/jolalinotes/ui/viewstate/LoginViewState.kt
+++ app/src/main/java/com/erikriosetiawan/jolalinotes/ui/viewstate/LoginViewState.kt
@@ -3,5 +3,6 @@ package com.erikriosetiawan.jolalinotes.ui.viewstate
 data class LoginViewState(
     var loading: Boolean = false,
     var responseCode: Int? = null,
-    var token: String? = null
+    var token: String? = null,
+    var exception: Exception? = null
 )
