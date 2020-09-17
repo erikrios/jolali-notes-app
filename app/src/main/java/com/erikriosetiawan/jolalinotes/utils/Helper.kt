52e7cb656030630b0cc466c71ec9a321fecca9d6
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-17 12:33:25 +0700

    Add getToken high order function

--- app/src/main/java/com/erikriosetiawan/jolalinotes/utils/Helper.kt
+++ app/src/main/java/com/erikriosetiawan/jolalinotes/utils/Helper.kt
@@ -1,11 +1,23 @@
 package com.erikriosetiawan.jolalinotes.utils
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
 
 fun AppCompatActivity.setCustomActionBar(toolbar: Toolbar?, title: String? = "") {
     this.apply {
         setSupportActionBar(toolbar)
         supportActionBar?.title = title
     }
+}
+
+/**
+ * Get token from shared preferences
+ */
+fun Fragment.getToken(): String? {
+    val sharedPref: SharedPreferences? =
+        activity?.getSharedPreferences(Constant.PREF_FILE_KEY, Context.MODE_PRIVATE)
+    return sharedPref?.getString(Constant.PREF_AUTH_TOKEN, "")
 }
