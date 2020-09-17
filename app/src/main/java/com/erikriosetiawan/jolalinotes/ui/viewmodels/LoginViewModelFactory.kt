48754c15e405be90621af0b5731d313b2c47dba6
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-17 10:12:00 +0700

    Create LoginViewModelFactory.kt

---
+++ app/src/main/java/com/erikriosetiawan/jolalinotes/ui/viewmodels/LoginViewModelFactory.kt
@@ -0,0 +1,17 @@
+package com.erikriosetiawan.jolalinotes.ui.viewmodels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.erikriosetiawan.jolalinotes.repository.NotesRepository
+
+@Suppress("UNCHECKED_CAST")
+class LoginViewModelFactory(private val repository: NotesRepository) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(LoginViewModelFactory::class.java)) {
+            return LoginViewModel(repository) as T
+        }
+
+        throw IllegalArgumentException()
+    }
+}
