c697de0ac0540b6207d5e969d25dea8f47340f26
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-17 18:50:15 +0700

    Refactoring the code

--- app/src/main/java/com/erikriosetiawan/jolalinotes/ui/fragments/LoginFragment.kt
+++ app/src/main/java/com/erikriosetiawan/jolalinotes/ui/fragments/LoginFragment.kt
@@ -3,7 +3,6 @@ package com.erikriosetiawan.jolalinotes.ui.fragments
 import android.content.Context.MODE_PRIVATE
 import android.os.Bundle
 import android.text.Html
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
