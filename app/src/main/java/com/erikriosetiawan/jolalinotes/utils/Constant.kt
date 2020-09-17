afdfe130ef80326441e778703cdb447590ba31df
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-17 11:09:05 +0700

    Create Constant.kt

--- app/src/main/java/com/erikriosetiawan/jolalinotes/utils/Constant.kt
+++ app/src/main/java/com/erikriosetiawan/jolalinotes/utils/Constant.kt
@@ -1,5 +1,6 @@
 package com.erikriosetiawan.jolalinotes.utils
 
 object Constant {
-    const val PREF_TOKEN = "jolali-notes-token"
+    const val PREF_FILE_KEY = "com.erikriosetiawan.jolalinotes.PREFERENCE_FILE_KEY"
+    const val PREF_AUTH_TOKEN = "jolali-notes-auth-token"
 }
