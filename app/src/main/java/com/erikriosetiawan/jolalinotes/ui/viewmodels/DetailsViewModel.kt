6094574d7b8cae9cf4be9943c6b485125ff79ffd
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-18 19:52:05 +0700

    Create DetailsViewModel.kt

---
+++ app/src/main/java/com/erikriosetiawan/jolalinotes/ui/viewmodels/DetailsViewModel.kt
@@ -0,0 +1,226 @@
+package com.erikriosetiawan.jolalinotes.ui.viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.erikriosetiawan.jolalinotes.repository.NotesRepository
+import com.erikriosetiawan.jolalinotes.ui.viewstate.DetailsViewState
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+
+class DetailsViewModel(private val repository: NotesRepository, private val token: String) :
+    ViewModel() {
+
+    private val _viewState = MutableLiveData<DetailsViewState>().apply {
+        value = DetailsViewState(loading = true)
+    }
+
+    val viewState: LiveData<DetailsViewState>
+        get() = _viewState
+
+    fun getNote(id: String): Job {
+        return viewModelScope.launch {
+            _viewState.value = DetailsViewState(loading = true)
+
+            try {
+                val response = repository.getNote(id, token)
+                when {
+                    response.isSuccessful -> {
+                        _viewState.value = DetailsViewState(
+                            loading = false,
+                            responseCode = response.code(),
+                            note = response.body()
+                        )
+                    }
+                    response.code() == 401 -> {
+                        _viewState.value = DetailsViewState(
+                            loading = false,
+                            responseCode = response.code(),
+                            exception = Exception("Access denied. No token provided.")
+                        )
+                    }
+                    response.code() == 400 -> {
+                        _viewState.value = DetailsViewState(
+                            loading = false,
+                            responseCode = response.code(),
+                            exception = Exception("Invalid token.")
+                        )
+                    }
+                    response.code() == 404 -> {
+                        _viewState.value = DetailsViewState(
+                            loading = false,
+                            responseCode = response.code(),
+                            exception = Exception("Note with given id was not found.")
+                        )
+                    }
+                    response.code() == 500 -> {
+                        _viewState.value = DetailsViewState(
+                            loading = false,
+                            responseCode = response.code(),
+                            exception = Exception("Internal server error.")
+                        )
+                    }
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+                _viewState.value = DetailsViewState(
+                    loading = false,
+                    exception = Exception(e.message)
+                )
+            }
+        }
+    }
+
+    fun createNote(title: String, description: String): Job {
+        return viewModelScope.launch {
+            _viewState.value = DetailsViewState(loading = true)
+
+            try {
+                val response = repository.createNote(token, title, description)
+                when {
+                    response.isSuccessful -> {
+                        _viewState.value = DetailsViewState(
+                            loading = false,
+                            responseCode = response.code(),
+                            note = response.body()
+                        )
+                    }
+                    response.code() == 401 -> {
+                        _viewState.value = DetailsViewState(
+                            loading = false,
+                            responseCode = response.code(),
+                            exception = Exception("Access denied. No token provided.")
+                        )
+                    }
+                    response.code() == 400 -> {
+                        _viewState.value = DetailsViewState(
+                            loading = false,
+                            responseCode = response.code(),
+                            exception = Exception("Invalid token.")
+                        )
+                    }
+                    response.code() == 500 -> {
+                        _viewState.value = DetailsViewState(
+                            loading = false,
+                            responseCode = response.code(),
+                            exception = Exception("Internal server error.")
+                        )
+                    }
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+                _viewState.value = DetailsViewState(
+                    loading = false,
+                    exception = Exception(e.message)
+                )
+            }
+        }
+    }
+
+    fun updateNote(id: String, title: String, description: String): Job {
+        return viewModelScope.launch {
+            _viewState.value = DetailsViewState(loading = true)
+
+            try {
+                val response = repository.updateNote(id, token, title, description)
+                when {
+                    response.isSuccessful -> {
+                        _viewState.value = DetailsViewState(
+                            loading = false,
+                            responseCode = response.code(),
+                            note = response.body()
+                        )
+                    }
+                    response.code() == 401 -> {
+                        _viewState.value = DetailsViewState(
+                            loading = false,
+                            responseCode = response.code(),
+                            exception = Exception("Access denied. No token provided.")
+                        )
+                    }
+                    response.code() == 400 -> {
+                        _viewState.value = DetailsViewState(
+                            loading = false,
+                            responseCode = response.code(),
+                            exception = Exception("Invalid token.")
+                        )
+                    }
+                    response.code() == 404 -> {
+                        _viewState.value = DetailsViewState(
+                            loading = false,
+                            responseCode = response.code(),
+                            exception = Exception("Note with given id was not found..")
+                        )
+                    }
+                    response.code() == 500 -> {
+                        _viewState.value = DetailsViewState(
+                            loading = false,
+                            responseCode = response.code(),
+                            exception = Exception("Internal server error.")
+                        )
+                    }
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+                _viewState.value = DetailsViewState(
+                    loading = false,
+                    exception = Exception(e.message)
+                )
+            }
+        }
+    }
+
+    fun deleteNote(id: String): Job {
+        return viewModelScope.launch {
+            _viewState.value = DetailsViewState(loading = true)
+
+            try {
+                val response = repository.deleteNote(id, token)
+                when {
+                    response.isSuccessful -> {
+                        _viewState.value = DetailsViewState(
+                            loading = false,
+                            responseCode = response.code(),
+                            note = response.body()
+                        )
+                    }
+                    response.code() == 401 -> {
+                        _viewState.value = DetailsViewState(
+                            loading = false,
+                            responseCode = response.code(),
+                            exception = Exception("Access denied. No token provided.")
+                        )
+                    }
+                    response.code() == 400 -> {
+                        _viewState.value = DetailsViewState(
+                            loading = false,
+                            responseCode = response.code(),
+                            exception = Exception("Invalid token.")
+                        )
+                    }
+                    response.code() == 404 -> {
+                        _viewState.value = DetailsViewState(
+                            loading = false,
+                            responseCode = response.code(),
+                            exception = Exception("Note with given id was not found..")
+                        )
+                    }
+                    response.code() == 500 -> {
+                        _viewState.value = DetailsViewState(
+                            loading = false,
+                            responseCode = response.code(),
+                            exception = Exception("Internal server error.")
+                        )
+                    }
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+                _viewState.value = DetailsViewState(
+                    loading = false,
+                    exception = Exception(e.message)
+                )
+            }
+        }
+    }
+}
