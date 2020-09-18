617851082452d544de7a4b503bf28ee6470a2d1d
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-18 17:51:55 +0700

    Create DashboardViewModelProvider.kt

---
+++ app/src/main/java/com/erikriosetiawan/jolalinotes/ui/viewmodels/DashboardViewModelProvider.kt
@@ -0,0 +1,21 @@
+package com.erikriosetiawan.jolalinotes.ui.viewmodels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.erikriosetiawan.jolalinotes.repository.NotesRepository
+
+@Suppress("UNCHECKED_CAST")
+class DashboardViewModelProvider(
+    private val repository: NotesRepository,
+    private val token: String
+) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+
+        if (modelClass.isAssignableFrom(DashboardViewModel::class.java)) {
+            return DashboardViewModel(repository, token) as T
+        }
+
+        throw IllegalArgumentException()
+    }
+}
