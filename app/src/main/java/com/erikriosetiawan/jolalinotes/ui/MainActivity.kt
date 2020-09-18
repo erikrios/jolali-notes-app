f5e28777ab6d274954ceb4b76fb5d0d9d40bb2e5
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-18 13:41:51 +0700

    Enable the StartApp test ads

--- app/src/main/java/com/erikriosetiawan/jolalinotes/ui/MainActivity.kt
+++ app/src/main/java/com/erikriosetiawan/jolalinotes/ui/MainActivity.kt
@@ -2,23 +2,24 @@ package com.erikriosetiawan.jolalinotes.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.erikriosetiawan.jolalinotes.BuildConfig
 import com.erikriosetiawan.jolalinotes.databinding.ActivityMainBinding
-import com.erikriosetiawan.jolalinotes.repository.NotesRepository
 import com.erikriosetiawan.jolalinotes.utils.Constant.PREF_FILE_KEY
+import com.startapp.sdk.adsbase.StartAppSDK
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private lateinit var repository: NotesRepository
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+
+        // TODO Always use test ads during development and testing
+        StartAppSDK.setTestAdsEnabled(BuildConfig.DEBUG)
+
         setContentView(binding.root)
 
         getSharedPreferences(PREF_FILE_KEY, MODE_PRIVATE)
-
-        repository = NotesRepository()
     }
 }
